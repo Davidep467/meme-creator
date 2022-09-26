d93f58803c32976a9fb0dae33f24fc422b02908a
Davidep467 <65339632+Davidep467@users.noreply.github.com>
2022-09-26 12:29:52 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
    
-  // nei beatles sistemare il +/- come nell'addbox qui, è giusto ceh ci siano così  tante cose per ogni box?
+
   return (
 <div>
       <HeaderBar />
